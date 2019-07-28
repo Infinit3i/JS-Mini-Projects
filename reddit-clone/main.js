04cd7e7149c4a63d4f77e28bea9cde215e49d84b
tech-guardian-security <matthewiver@protonmail.com>
2019-07-28 13:41:00 -0500

    window sizing

--- reddit-clone/main.js
+++ reddit-clone/main.js
@@ -7,8 +7,8 @@ let win
 function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1600,
+    height: 1200,
     webPreferences: {
       nodeIntegration: true
     }
